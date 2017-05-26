f814b5fd4cf04d84854ae817cffec8427c4f3cbf
Ju Liu <ju.liu@welaika.com>
2017-05-26 23:17:48 +0100

    Add tests for loop and translator

---
+++ loop_test.exs
@@ -0,0 +1,44 @@
+ExUnit.start
+Code.require_file("loop.ex", __DIR__)
+
+defmodule LoopTest do
+  use ExUnit.Case
+  import Loop
+
+  test "Is it really that easy?" do
+    assert Code.ensure_loaded?(Loop)
+  end
+
+  test "while/2 loops as long as expression is truthy" do
+    pid = spawn(fn -> Process.sleep(:infinity) end)
+
+    send self(), :one
+    while Process.alive?(pid) do
+      receive do
+        :one -> send self(), :two
+        :two -> send self(), :three
+        :three ->
+          Process.exit(pid, :kill)
+          send self(), :done
+      end
+    end
+
+    assert_received :done
+  end
+
+  test "break/0 breaks execution" do
+    send self(), :one
+
+    while true do
+      receive do
+        :one -> send self(), :two
+        :two -> send self(), :three
+        :three ->
+          send self(), :done
+          break()
+      end
+    end
+
+    assert_received :done
+  end
+end
---
+++ translator_test.exs
@@ -0,0 +1,88 @@
+ExUnit.start
+Code.require_file("translator.ex", __DIR__)
+
+defmodule LoopTest do
+  use ExUnit.Case
+
+  defmodule Example do
+    use Translator
+
+    locale "en",
+      foo: "bar",
+      flash: [
+        hello: "Hello %{first} %{last}!",
+        bye: "Bye, %{name}!"
+      ],
+      users: [
+        title: "Users"
+      ]
+
+    locale "fr",
+      flash: [
+        hello: "Salut %{first} %{last}!",
+        bye: "Au revoir, %{name}!"
+      ],
+      users: [
+        title: "Utilisateurs"
+      ]
+  end
+
+  test "t/3 works for root level translations" do
+    assert Example.t("en", "foo") == "bar"
+  end
+
+  test "t/3 walks through all levels" do
+    assert Example.t("en", "users.title") == "Users"
+  end
+
+  test "t/3 allows multiple locales to be registered" do
+    assert Example.t("en", "users.title") == "Users"
+    assert Example.t("fr", "users.title") == "Utilisateurs"
+  end
+
+  test "t/3 interpolates bindings" do
+    assert Example.t("en", "flash.hello", first: "Ju", last: "Liu") ==
+      "Hello Ju Liu!"
+  end
+
+  test "t/3 raises errors when binding is not provided" do
+    assert_raise KeyError, fn -> Example.t("en", "flash.hello", first: "Ju") end
+  end
+
+  test "t/3 returns {:error, :no_translation} when it's not available" do
+    assert Example.t("fr", "foo") == {:error, :no_translation}
+  end
+
+  test "t/3 converts values to strings before interpolating" do
+    assert Example.t("en", "flash.hello", first: 1, last: 2) == "Hello 1 2!"
+  end
+
+  test "compile/1 generates catch all t/3 functions" do
+    assert Translator.compile([]) |> Macro.to_string == String.strip ~S"""
+    (
+      def(t(locale, path, bindings \\ []))
+      []
+      def(t(_locale, _path, _bindings)) do
+        {:error, :no_translation}
+      end
+    )
+    """
+  end
+
+  test "compile/1 generates t/3 functions for each local" do
+    locales = [{"en", [foo: "bar", bar: "%{baz}"]}]
+    assert Translator.compile(locales) |> Macro.to_string == String.strip ~S"""
+    (
+      def(t(locale, path, bindings \\ []))
+      [[def(t("en", "foo", bindings)) do
+        "" <> "bar"
+      end, def(t("en", "bar", bindings)) do
+        (("" <> "") <> to_string(Keyword.fetch!(bindings, :baz))) <> ""
+      end]]
+      def(t(_locale, _path, _bindings)) do
+        {:error, :no_translation}
+      end
+    )
+    """
+  end
+end
