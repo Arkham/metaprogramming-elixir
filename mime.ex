3c74270e74276050ba8cfc112474420513eb69de
Ju Liu <ju.liu@welaika.com>
2017-05-26 12:08:47 +0100

    Add macro to Mime module

--- mime.ex
+++ mime.ex
@@ -8,11 +8,27 @@ defmodule Mime do
       |> Enum.map(&String.strip/1)
     extensions = String.split(rest, ~r/,\s?/)
 
-    def exts_from_type(unquote(type)), do: unquote(extensions)
-    def type_from_ext(ext) when ext in unquote(extensions), do: unquote(type)
+    def exts_for_type(unquote(type)), do: unquote(extensions)
+    def type_for_ext(ext) when ext in unquote(extensions), do: unquote(type)
   end
 
-  def exts_from_type(_type), do: []
-  def type_from_ext(_ext), do: nil
-  def valid_type?(type), do: exts_from_type(type) |> Enum.any?
+  def exts_for_type(_type), do: []
+  def type_for_ext(_ext), do: nil
+  def valid_type?(type), do: exts_for_type(type) |> Enum.any?
+
+  defmacro __using__(additional) do
+    additional_ast = for {type, extensions} <- additional do
+      type = to_string(type)
+      quote do
+        def exts_for_type(unquote(type)), do: unquote(extensions)
+        def type_for_ext(ext) when ext in unquote(extensions), do: unquote(type)
+      end
+    end
+
+    quote do
+      unquote(additional_ast)
+      defdelegate exts_for_type(type), to: Mime
+      defdelegate type_for_ext(ext), to: Mime
+    end
+  end
 end
